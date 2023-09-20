b278f0f16c41b1cb87b0e0a604f1fdeb174f88ae
Syed Tasavour <syedtasavour86@gmail.com>
2023-09-20 22:52:32 +0530

    ExamResultMessage.java

---
+++ Day 2/ExamResultMessage.java
@@ -0,0 +1,154 @@
+import java.util.*;
+
+public class ExamResultMessage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter their name
+        System.out.print("Enter Your Name: ");
+        String name = scanner.nextLine().toLowerCase();  // Convert the input to lowercase for case-insensitive comparison
+
+        // Switch statement to provide exam result messages based on the name
+        switch (name) {
+            case "studentname1":
+                System.out.println("Hi StudentName1! You passed the exam. Well done!");
+                break;
+            case "studentname2":
+                System.out.println("Hi StudentName2! You failed the exam. Keep studying and try again!");
+                break;
+            case "studentname3":
+                System.out.println("Hi StudentName3! You passed the exam. Great job!");
+                break;
+            case "studentname4":
+                System.out.println("Hi StudentName4! You failed the exam. Don't worry, keep trying!");
+                break;
+            case "studentname5":
+                System.out.println("Hi StudentName5! You passed the exam. Congratulations!");
+                break;
+            case "studentname6":
+                System.out.println("Hi StudentName6! You failed the exam. Don't give up, keep striving!");
+                break;
+            case "studentname7":
+                System.out.println("Hi StudentName7! You passed the exam. Good job!");
+                break;
+            case "studentname8":
+                System.out.println("Hi StudentName8! You failed the exam. Focus and try harder!");
+                break;
+            case "studentname9":
+                System.out.println("Hi StudentName9! You passed the exam. Well done!");
+                break;
+            case "studentname10":
+                System.out.println("Hi StudentName10! You failed the exam. Keep studying and try again!");
+                break;
+            case "studentname11":
+                System.out.println("Hi StudentName11! You passed the exam. Great job!");
+                break;
+            case "studentname12":
+                System.out.println("Hi StudentName12! You failed the exam. Don't worry, keep trying!");
+                break;
+            case "studentname13":
+                System.out.println("Hi StudentName13! You passed the exam. Congratulations!");
+                break;
+            case "studentname14":
+                System.out.println("Hi StudentName14! You failed the exam. Don't give up, keep striving!");
+                break;
+            case "studentname15":
+                System.out.println("Hi StudentName15! You passed the exam. Good job!");
+                break;
+            case "studentname16":
+                System.out.println("Hi StudentName16! You failed the exam. Focus and try harder!");
+                break;
+            case "studentname17":
+                System.out.println("Hi StudentName17! You passed the exam. Well done!");
+                break;
+            case "studentname18":
+                System.out.println("Hi StudentName18! You failed the exam. Keep studying and try again!");
+                break;
+            case "studentname19":
+                System.out.println("Hi StudentName19! You passed the exam. Great job!");
+                break;
+            case "studentname20":
+                System.out.println("Hi StudentName20! You failed the exam. Don't worry, keep trying!");
+                break;
+            case "studentname21":
+                System.out.println("Hi StudentName21! You passed the exam. Congratulations!");
+                break;
+            case "studentname22":
+                System.out.println("Hi StudentName22! You failed the exam. Don't give up, keep striving!");
+                break;
+            case "studentname23":
+                System.out.println("Hi StudentName23! You passed the exam. Good job!");
+                break;
+            case "studentname24":
+                System.out.println("Hi StudentName24! You failed the exam. Focus and try harder!");
+                break;
+            case "studentname25":
+                System.out.println("Hi StudentName25! You passed the exam. Well done!");
+                break;
+            case "studentname26":
+                System.out.println("Hi StudentName26! You failed the exam. Keep studying and try again!");
+                break;
+            case "studentname27":
+                System.out.println("Hi StudentName27! You passed the exam. Great job!");
+                break;
+            case "studentname28":
+                System.out.println("Hi StudentName28! You failed the exam. Don't worry, keep trying!");
+                break;
+            case "studentname29":
+                System.out.println("Hi StudentName29! You passed the exam. Congratulations!");
+                break;
+            case "studentname30":
+                System.out.println("Hi StudentName30! You failed the exam. Don't give up, keep striving!");
+                break;
+            case "studentname31":
+                System.out.println("Hi StudentName31! You passed the exam. Good job!");
+                break;
+            case "studentname32":
+                System.out.println("Hi StudentName32! You failed the exam. Focus and try harder!");
+                break;
+            case "studentname33":
+                System.out.println("Hi StudentName33! You passed the exam. Well done!");
+                break;
+            case "studentname34":
+                System.out.println("Hi StudentName34! You failed the exam. Keep studying and try again!");
+                break;
+            case "studentname35":
+                System.out.println("Hi StudentName35! You passed the exam. Great job!");
+                break;
+            case "studentname36":
+                System.out.println("Hi StudentName36! You failed the exam. Don't worry, keep trying!");
+                break;
+            case "studentname37":
+                System.out.println("Hi StudentName37! You passed the exam. Congratulations!");
+                break;
+            case "studentname38":
+                System.out.println("Hi StudentName38! You failed the exam. Don't give up, keep striving!");
+                break;
+            case "studentname39":
+                System.out.println("Hi StudentName39! You passed the exam. Good job!");
+                break;
+            case "studentname40":
+                System.out.println("Hi StudentName40! You failed the exam. Focus and try harder!");
+                break;
+            case "studentname41":
+                System.out.println("Hi StudentName41! You passed the exam. Well done!");
+                break;
+            case "studentname42":
+                System.out.println("Hi StudentName42! You failed the exam. Keep studying and try again!");
+                break;
+            case "studentname43":
+                System.out.println("Hi StudentName43! You passed the exam. Great job!");
+                break;
+            case "studentname44":
+                System.out.println("Hi StudentName44! You failed the exam. Don't worry, keep trying!");
+                break;
+            case "studentname45":
+                System.out.println("Hi StudentName45! You passed the exam. Congratulations!");
+                break;
+            default:
+                // If the name doesn't match any case
+                System.out.println("Hello, " + name + "! We don't have your exam result.");
+                break;
+        }
+    }
+}
