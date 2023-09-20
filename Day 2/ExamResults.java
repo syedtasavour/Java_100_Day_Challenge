f6469939454a6e996732ddcc5dffb12a7ef223b1
Syed Tasavour <syedtasavour86@gmail.com>
2023-09-20 23:20:12 +0530

    ExamResults.java

---
+++ Day 2/ExamResults.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ExamResults {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get marks for each subject
+        System.out.print("Enter marks for Subject One out of 100: ");
+        float subject1Marks = scanner.nextFloat();
+
+        System.out.print("Enter marks for Subject Two out of 100: ");
+        float subject2Marks = scanner.nextFloat();
+
+        System.out.print("Enter marks for Subject Three out of 100: ");
+        float subject3Marks = scanner.nextFloat();
+
+        // Calculate the total percentage
+        double totalPercentage = (subject1Marks + subject2Marks + subject3Marks) * 100 / (100 * 3);
+
+        // Check if the student passed all subjects
+        if (subject1Marks >= 33 && subject2Marks >= 33 && subject3Marks >= 33) {
+            System.out.println("Congratulations! You have passed all subjects with the following percentages:\n" +
+                    "Subject 1: " + subject1Marks + "\nSubject 2: " + subject2Marks + "\nSubject 3: " + subject3Marks +
+                    "\nTotal Percentage: " + totalPercentage);
+        } else {
+            System.out.println("You have not passed all the subjects. Here are the percentages:\n" +
+                    "Subject 1: " + subject1Marks + "\nSubject 2: " + subject2Marks + "\nSubject 3: " + subject3Marks +
+                    "\nTotal Percentage: " + totalPercentage);
+        }
+    }
+}
