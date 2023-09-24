29f1333d3b2a4d64c71e58c529cd453fcc31968d
Syed Tasavour <syedtasavour86@gmail.com>
2023-09-25 01:15:46 +0530

    Update TwoDimensionalArrayExample.java

--- Day 6/TwoDimensionalArrayExample.java
+++ Day 6/TwoDimensionalArrayExample.java
@@ -1,4 +1,4 @@
-public class TwoDimensionalArrayExample {
+public class TwoDimensionalArrayPrint {
     public static void main(String[] args) {
         int[][] array;  // Declare a 2D array
         array = new int[2][3];  // Initialize the 2D array with 2 rows and 3 columns
@@ -11,11 +11,12 @@ public class TwoDimensionalArrayExample {
         array[1][1] = 14;
         array[1][2] = 15;
 
-        // Print the elements of the 2D array
+        // Print the elements of the 2D array with proper formatting
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                System.out.println("Element at index [" + i + "][" + j + "]: " + array[i][j]);
+                System.out.print(array[i][j] + "  ");  // Print each element with space
             }
+            System.out.println();  // Move to the next line after each row
         }
     }
 }
