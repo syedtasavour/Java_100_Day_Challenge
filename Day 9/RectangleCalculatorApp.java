fe047ce75ba5e36f0b6364b29bf8b4ded9c61ae2
Syed Tasavour <syedtasavour86@gmail.com>
2023-09-28 05:28:42 +0530

    Update RectangleCalculatorApp.java

--- Day 9/RectangleCalculatorApp.java
+++ Day 9/RectangleCalculatorApp.java
@@ -1,22 +1,32 @@
-class Rectangle{
-    int a,b;
-    public int Area (){
-        return a*b;
+class Rectangle {
+    // Length and width of the rectangle
+    int length;
+    int width;
+
+    // Method to calculate the area of the rectangle
+    public int calculateArea() {
+        return length * width;
     }
-    public int Parameter (){
-        return 2*(a+b);
+
+    // Method to calculate the perimeter of the rectangle
+    public int calculatePerimeter() {
+        return 2 * (length + width);
     }
 }
 
-
-
-
-public class RectangleCalculator {
+public class RectangleCalculatorApp {
     public static void main(String[] args) {
-        Rectangle CallMethod = new Rectangle();
-        CallMethod.a = 5;
-        CallMethod.b = 5;
-        System.out.println(CallMethod.Area());
-        System.out.println(CallMethod.Parameter());
+        // Create an instance of the Rectangle class
+        Rectangle rectangle = new Rectangle();
+
+        // Set the dimensions of the rectangle
+        rectangle.length = 5;
+        rectangle.width = 5;
+
+        // Calculate and print the area of the rectangle
+        System.out.println("Area of the rectangle: " + rectangle.calculateArea());
+
+        // Calculate and print the perimeter of the rectangle
+        System.out.println("Perimeter of the rectangle: " + rectangle.calculatePerimeter());
     }
 }
