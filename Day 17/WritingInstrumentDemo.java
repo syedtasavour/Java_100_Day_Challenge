6f5490f83aa87fe61bd1cfd124550b38dc08d1d3
Syed Tasavour <syedtasavour86@gmail.com>
2023-10-14 11:43:41 -0700

    Create WritingInstrumentDemo.java

---
+++ Day 17/WritingInstrumentDemo.java
@@ -0,0 +1,23 @@
+abstract class WritingInstrument {
+    abstract public void write(); // Abstract method to write
+    abstract public void refill(); // Abstract method to refill
+}
+
+class Pen extends WritingInstrument {
+    public void write() {
+        System.out.println("Writing");
+    }
+
+    public void refill() {
+        // Implement refill functionality
+        System.out.println("Refilling pen.");
+    }
+}
+
+public class WritingInstrumentDemo {
+    public static void main(String[] args) {
+        Pen penInstance = new Pen();  // Create an instance of the Pen subclass
+        penInstance.write();  // Invoke the write method
+        penInstance.refill();  // Invoke the refill method
+    }
+}
