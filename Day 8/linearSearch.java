ebce7ae0b24e07879ec69fb4ac86cb9fb018d736
Syed Tasavour <syedtasavour86@gmail.com>
2024-03-27 11:54:35 +0530

    a

---
+++ Day 8/linearSearch.java
@@ -0,0 +1,15 @@
+public class linearSearch {
+    public static int lin(int arr[],int key){
+        for(int i=0;i<arr.length;i++){
+            if(key ==arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int m[]= {1,22,4,5,6,7,8564,34,53};
+        int key =3;
+        System.out.print(lin(m,key));
+    }
+}
